39736333637d3a9095d5843cd4a5ed7ed70ef531
= <=>
2025-12-06 10:18:49 +0530

    null or empty helper methods added

--- src/main/java/com/helper/bootcommons/utils/Util.java
+++ src/main/java/com/helper/bootcommons/utils/Util.java
@@ -1,5 +1,9 @@
 package com.helper.bootcommons.utils;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Util {
     public static void delay(int seconds) {
         try {
@@ -7,5 +11,21 @@ public class Util {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    public static boolean nullOrEmpty(String string) {
+        return string == null || string.isEmpty();
+    }
+    
+    public static <T> boolean nullOrEmpty(List<T> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static <K, V> boolean nullOrEmpty(Map<K, V> map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static <T> boolean nullOrEmpty(Set<T> set) {
+        return set == null || set.isEmpty();
     }
 }
