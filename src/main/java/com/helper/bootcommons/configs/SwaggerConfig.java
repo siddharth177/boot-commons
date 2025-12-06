6ab4ed6a18eb233d6a5ecfe4af0a99c91c60f37a
= <=>
2025-12-06 10:42:56 +0530

    setting up auto swagger configuration

---
+++ src/main/java/com/helper/bootcommons/configs/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.helper.bootcommons.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(
+            @Value("${bootcommons.swagger.title}") String title,
+            @Value("${bootcommons.swagger.description}") String description,
+            @Value("${bootcommons.swagger.version}") String version,
+            @Value("${bootcommons.swagger.contact.name}") String contactName,
+            @Value("${bootcommons.swagger.contact.email}") String contactEmail) {
+
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.getInfo().setTitle(title);
+        openAPI.getInfo().setDescription(description);
+        openAPI.getInfo().setVersion(version);
+        openAPI.getInfo().setContact(new io.swagger.v3.oas.models.info.Contact());
+        openAPI.getInfo().getContact().setEmail(contactEmail);
+        openAPI.getInfo().getContact().setName(contactName);
+        return openAPI;
+    }
+
+    @Bean
+    public List<GroupedOpenApi> apiGroups(@Value("${bootcommons.swagger.groups}") List<String> groups) {
+        return groups.stream()
+                .map(group -> GroupedOpenApi.builder()
+                        .group(group)
+                        .pathsToMatch("/**")
+                        .build())
+                .toList();
+    }
+}
